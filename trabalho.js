bd50add1200919a1fef52c69763a2830e8e49e93
RoniChr <ronaldoliveira108@gmail.com>
2025-05-16 23:13:36 -0300

    ajustes

---
+++ trabalho.js
@@ -0,0 +1,2 @@
+let botao = document.querySelector("#botao");
+botao.addEventListener("click", function () { document.getElementById("mensagem").innerText = "A solidão é como o eco.. Ele só responde quando não há mais ninguém por perto"; })
